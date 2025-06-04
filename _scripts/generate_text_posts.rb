c6380fdfba32d4e86e2b3d3389de2fe36d6fd66c
k <sample@example.com>
2025-06-04 17:22:14 +0900

    Merge origin/main into main
    # Conflicts:
    #	.github/workflows/generate-proj-dir-tree.yml

--- _scripts/generate_text_posts.rb
+++ _scripts/generate_text_posts.rb
@@ -1,17 +1,22 @@
 require 'fileutils'
 require 'time'
+require 'kramdown'
 
 POSTS_DIR = "_posts/ja"
 OUTPUT_DIR = "_pages/text/ja"
 
 def extract_metadata(dir_name)
-  # 例: 2025-05-30-T135040+0900-UUID-ja
   if dir_name =~ /^(\d{4}-\d{2}-\d{2})-T(\d{6})([+-]\d{4})-(.+)-ja$/
     date = "#{$1}T#{$2[0..1]}:#{$2[2..3]}:#{$2[4..5]}#{$3}"
     uuid = "#{$1}-T#{$2}#{$3}-#{$4}"
     return [uuid, date]
   end
   nil
+end
+
+def extract_body_without_front_matter(file_path)
+  content = File.read(file_path, encoding: 'utf-8')
+  content.sub(/\A---\s*.*?---\s*/m, '') # YAML front matter を除去
 end
 
 Dir.entries(POSTS_DIR).each do |dir_name|
@@ -24,11 +29,14 @@ Dir.entries(POSTS_DIR).each do |dir_name|
   post_file = File.join(POSTS_DIR, dir_name, "#{uuid}-ja.md")
   next unless File.exist?(post_file)
 
+  body_md = extract_body_without_front_matter(post_file)
+  body_html = Kramdown::Document.new(body_md).to_html
+
   output_path = File.join(OUTPUT_DIR, uuid, "index.html")
   FileUtils.mkdir_p(File.dirname(output_path))
 
   File.open(output_path, "w:utf-8") do |f|
-    f.puts <<~MARKDOWN
+    f.puts <<~HTML
       ---
       layout: text-post
       lang: ja
@@ -38,11 +46,7 @@ Dir.entries(POSTS_DIR).each do |dir_name|
       date: #{iso_date}
       ---
 
-      {% capture original %}
-        {% include_relative ../../../_posts/ja/#{uuid}/#{uuid}-ja.md %}
-      {% endcapture %}
-      {{ original | markdownify }}
-    MARKDOWN
+      #{body_html}
+    HTML
   end
 end
-
