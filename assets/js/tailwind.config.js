e0aca76cca6a804b294272704f3bce2ef66ac1ec
k <sample@example.com>
2025-06-17 15:41:21 +0900

    Merge origin/main into main
    # Conflicts:
    #	.github/workflows/generate-proj-dir-tree.yml

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./**/*.html"],
+  safelist: [
+    "text-blue-600", "text-red-600", "text-green-600", "text-yellow-600",
+    "text-indigo-600", "text-purple-600", "text-pink-600", "text-gray-600",
+    "text-orange-600", "text-teal-600",
+    "bg-blue-100", "bg-red-100", "bg-green-100", "bg-yellow-100",
+    "bg-indigo-100", "bg-purple-100", "bg-pink-100", "bg-gray-100",
+    "bg-orange-100", "bg-teal-100",
+    "text-blue-800", "text-red-800", "text-green-800", "text-yellow-800",
+    "text-indigo-800", "text-purple-800", "text-pink-800", "text-gray-800",
+    "text-orange-800", "text-teal-800"
+  ],
+};
